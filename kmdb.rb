53cd72ec9260723d6a4570ff8fdcf86f25ba9df2
Christian Borges <christian.borges@kellogg.northwestern.edu>
2025-02-12 01:38:25 +0000

    Models created and migrated

--- kmdb.rb
+++ kmdb.rb
@@ -85,18 +85,20 @@
 # TODO!
 
 # Prints a header for the movies output
-puts "Movies"
-puts "======"
-puts ""
+# puts "Movies"
+# puts "======"
+# puts ""
 
 # Query the movies data and loop through the results to display the movies output.
 # TODO!
 
 # Prints a header for the cast output
-puts ""
-puts "Top Cast"
-puts "========"
-puts ""
+# puts ""
+# puts "Top Cast"
+# puts "========"
+# puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
 # TODO!
+
+
